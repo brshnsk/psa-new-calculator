e6942f7920d13343a5f5c117690dbcbb50c1b4eb
brshnsk <brshnsk@gmail.com>
2023-11-13 16:41:30 -0400

    [Fix]

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],  base: "/psa-new-calculator",
+  plugins: [react()],  base: "/psa-new-calculator/",
 })
