ca982775f08403868be22e8fee1bbc09e7d2bcdd
brshnsk <brshnsk@gmail.com>
2023-11-13 15:00:32 -0400

    [Fix]

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,19 +1,15 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import { SenConverter } from "../pages/SenConverter"
-import { App } from "../App"
-
+import { Navigate, Route, Routes } from "react-router-dom";
+import { SenConverter } from "../pages/SenConverter";
+import { App } from "../App";
 
 export const AppRouter = () => {
   return (
 
-  
-    <Routes>
-        <Route path="/sen" element={<SenConverter />} />
+      <Routes>
+        <Route path="/sen" element={<SenConverter/>} />
         <Route path="/psa" element={<App />} />
-        <Route path="/*" element={<Navigate to="/psa" />} />
-
-
+        <Route path="/" element={<Navigate to="/psa" />} />
       </Routes>
 
-  )
-}
+  );
+};
